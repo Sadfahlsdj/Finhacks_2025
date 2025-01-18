e173f7b7fd6b8c01f3998e162840159ba50c47db
Sadfahlsdj's mac <124646333+Sadfahlsdj@users.noreply.github.com>
2025-01-18 16:25:00 -0500

    swapping to a larger dataset, df_editing is used to aggregate the dataframe used

--- df_editing.py
+++ df_editing.py
@@ -1,13 +1,29 @@
-"""
-heavy WIP
-"""
-
 import pandas as pd
+from datasets import load_dataset
 
-df = pd.read_csv('amazon.csv')
-disc_percent = [float(int(d[:-1]) / 100) for d in df['discount_percentage']]
-df['discount_percentage'] = disc_percent
+partials_review = []
+partials_item = []
 
-df.to_csv('amazon_output.csv', index=False)
+with open('dataset_categories.txt') as f:
+    lines = [l.strip() for l in f.readlines()]
 
-print(df.head().to_string())
+for l in lines:
+    dataset_review = load_dataset("McAuley-Lab/Amazon-Reviews-2023", "raw_review_All_Beauty", split="full",
+                           trust_remote_code=True)
+    dataset_item = load_dataset("McAuley-Lab/Amazon-Reviews-2023", "raw_meta_All_Beauty", trust_remote_code=True)
+
+    partials_review.append(pd.DataFrame.from_dict(dataset_review)
+                           .sample(frac=0.002).reset_index(drop=True))
+    partials_item.append(pd.DataFrame.from_dict(dataset_item)
+                           .sample(frac=0.002).reset_index(drop=True))
+
+    print(f'category {l} done')
+
+df_review = pd.concat(partials_review)
+df_item = pd.concat(partials_item)
+
+print(df_review.head().to_string())
+print(df_item.head().to_string())
+
+df_review.to_csv('reviews_megaset.csv')
+df_item.to_csv('items_megaset.csv')
