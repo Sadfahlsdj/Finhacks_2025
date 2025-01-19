0f3d233c9f65b2d6cd9ff55d44c52ab21b52f1b3
Sadfahlsdj's mac <124646333+Sadfahlsdj@users.noreply.github.com>
2025-01-19 10:10:15 -0500

    new endpoint to grab sentiment, review score, & percentile based on parent_asin (id)

--- rest_api.py
+++ rest_api.py
@@ -1,14 +1,16 @@
 from flask import Flask, request, jsonify
 import pandas as pd
+from scipy import stats
+import json
 
 app = Flask(__name__)
 
 @app.get('/keyword')
 def get_by_keyword():
     """
-    keyword - keyword to search by
+    keyword - keyword to search by (currently searches main category & title (product name))
     amount - top # to return (default 50 probably)
-    :return:
+    :return: dataframe with top [amount] entries by predicted category matching the keyword
     """
     keyword = request.args.get('keyword')
     amount = int(request.args.get('amount'))
@@ -31,6 +33,13 @@ def get_by_keyword():
 
 @app.get('/stats')
 def get_stats_by_id():
+    """
+    id - id to search specific product for
+    :return: dataframe with 1 row with columns sentiment, review_score, and percentile
+        sentiment & review score are just pulled from value, percentile is percentile of the
+    predicted category value among entries with the same main category
+        percentile is liable to have wacky values if the main category has very few values
+    """
     id = request.args.get('id')
     df = pd.read_csv('items_megaset_with_categories.csv', index_col=False)
 
@@ -38,3 +47,10 @@ def get_stats_by_id():
     out = {}
     out['sentiment'] = info['sentiments']
     out['review_score'] = info['average_rating']
+
+    # percentile calculation
+    mc = info.iloc[0]['main_category'] # needed so it spits out correct thing
+    category_values = df[df['main_category'] == mc]
+    percentile = stats.percentileofscore(category_values['predicted_category'], info['predicted_category'])
+    out['percentile'] = round(percentile[0], 2) # percentile is a ndarray with 1 value
+    return pd.DataFrame(out).to_json(orient='records')
